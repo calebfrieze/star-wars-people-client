b06c89d7292996cb032d9d0e80a144c338482798
Caleb Frieze <calebfrieze@gmail.com>
2020-01-29 12:45:18 -0700

    ng init with people component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
+
+
+const routes: Routes = [
+  {path: '', component: PeopleComponent},
+  {path: 'people', component: PeopleComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
